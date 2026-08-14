5bb0366545f6ce17df9885c9b38ff05a3d0a1d95
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-08-14 20:20:51 +0200

    build(spwkit): expose simulator configuration in umbrella header #11

--- include/spwkit/spwkit.h
+++ include/spwkit/spwkit.h
@@ -3,6 +3,9 @@
 #define SPWKIT_SPWKIT_H
 
 #include "spwkit/api.h"
+#include "spwkit/config.h"
+#include "spwkit/types.h"
 #include "spwkit/port.h"
+#include "spwkit/simulator.h"
 
 #endif /* SPWKIT_SPWKIT_H */
