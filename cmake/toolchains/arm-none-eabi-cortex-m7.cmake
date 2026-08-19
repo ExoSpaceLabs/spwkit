068911784cd90a3a2638728968f13e89f9caabfc
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-08-19 16:32:05 +0200

    test(spwkit): add Cortex-M7 toolchain contract

---
+++ cmake/toolchains/arm-none-eabi-cortex-m7.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_RANLIB arm-none-eabi-ranlib)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_SIZE arm-none-eabi-size)
+
+set(SPWKIT_CORTEX_M7_FLAGS "-mcpu=cortex-m7 -mthumb -mfloat-abi=soft -ffunction-sections -fdata-sections")
+set(CMAKE_C_FLAGS_INIT "${SPWKIT_CORTEX_M7_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${SPWKIT_CORTEX_M7_FLAGS}")
+set(CMAKE_ASM_FLAGS_INIT "${SPWKIT_CORTEX_M7_FLAGS}")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE BOTH)
