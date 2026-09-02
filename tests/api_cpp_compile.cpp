56d75ff5cf1f010cd360017b122aa861a9b4f9f1
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-02 11:29:36 +0200

    test(spwkit): compile wrapper ownership parity

--- tests/api_cpp_compile.cpp
+++ tests/api_cpp_compile.cpp
@@ -26,8 +26,26 @@ static_assert(spwkit::version.minor == SPWKIT_TEST_PROJECT_VERSION_MINOR);
 static_assert(spwkit::version.patch == SPWKIT_TEST_PROJECT_VERSION_PATCH);
 static_assert(!std::is_copy_constructible_v<spwkit::Port>);
 static_assert(std::is_move_constructible_v<spwkit::Port>);
+static_assert(std::is_same_v<spwkit::Buffer, spw_buffer_t>);
+static_assert(std::is_same_v<spwkit::BufferView, spw_buffer_view_t>);
+static_assert(std::is_same_v<spwkit::WorkspaceRequirements,
+                             spw_port_workspace_requirements_t>);
 
 int spwkit_api_cpp_compile_probe() {
+    spw_port_config_t config = SPW_PORT_CONFIG_INITIALIZER(SPW_BACKEND_LOOPBACK);
+    spwkit::WorkspaceRequirements requirements{};
+    spwkit::Port port;
+    spwkit::Buffer* buffer = nullptr;
+    spwkit::BufferView view{};
+
+    (void)spwkit::Port::workspace_requirements(config, requirements);
+    (void)port.acquire_tx_buffer(1u, buffer);
+    if (buffer != nullptr) {
+        (void)spwkit::Port::buffer_view(*buffer, view);
+        (void)spwkit::Port::set_packet(*buffer, 0u);
+        (void)port.release_tx_buffer(buffer);
+    }
+
     return SPWKIT_API_VERSION_MAJOR == SPWKIT_TEST_PROJECT_VERSION_MAJOR &&
                    SPWKIT_API_VERSION_MINOR == SPWKIT_TEST_PROJECT_VERSION_MINOR &&
                    SPWKIT_API_VERSION_PATCH == SPWKIT_TEST_PROJECT_VERSION_PATCH
