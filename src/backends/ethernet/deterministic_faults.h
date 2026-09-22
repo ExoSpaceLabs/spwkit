4c528216dc833b101efee963e6d0b233cceaec4e
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2026-09-23 01:42:50 +0200

    refactor(spwkit): make fault model VSPW-internal

--- src/backends/ethernet/deterministic_faults.h
+++ src/backends/ethernet/deterministic_faults.h
@@ -4,30 +4,63 @@
 
 #include "backends/ethernet/vspw_tp.h"
 
-#include <spwkit/udp.h>
-
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#define SPW_VSPW_FAULT_RULE_COUNT 8u
+#define SPW_VSPW_FAULT_PROBABILITY_SCALE 10000u
+
+typedef uint8_t spw_vspw_fault_action_t;
+enum {
+    SPW_VSPW_FAULT_ACTION_NONE = 0u,
+    SPW_VSPW_FAULT_ACTION_TRANSPORT_DROP = 1u,
+    SPW_VSPW_FAULT_ACTION_TRANSPORT_DUPLICATE = 2u,
+    SPW_VSPW_FAULT_ACTION_TRANSPORT_REORDER = 3u,
+    SPW_VSPW_FAULT_ACTION_TRANSPORT_DELAY = 4u,
+    SPW_VSPW_FAULT_ACTION_SPACEWIRE_EEP = 5u
+};
+
+typedef uint8_t spw_vspw_fault_target_t;
+enum {
+    SPW_VSPW_FAULT_TARGET_ANY = 0u,
+    SPW_VSPW_FAULT_TARGET_DATA = 1u,
+    SPW_VSPW_FAULT_TARGET_TIME_CODE = 2u,
+    SPW_VSPW_FAULT_TARGET_ACK = 3u,
+    SPW_VSPW_FAULT_TARGET_KEEPALIVE = 4u
+};
+
+typedef struct spw_vspw_fault_rule {
+    spw_vspw_fault_action_t action;
+    spw_vspw_fault_target_t target;
+    uint16_t probability_per_10000;
+    uint32_t max_events;
+    uint32_t delay_us;
+    uint32_t reserved;
+} spw_vspw_fault_rule_t;
+
 typedef struct spw_fault_decision {
-    spw_udp_fault_action_t action;
+    spw_vspw_fault_action_t action;
     uint32_t delay_us;
 } spw_fault_decision_t;
 
 typedef struct spw_deterministic_fault_injector {
-    spw_udp_fault_rule_t rules[SPW_UDP_FAULT_RULE_COUNT];
-    uint64_t states[SPW_UDP_FAULT_RULE_COUNT];
-    uint32_t injected_counts[SPW_UDP_FAULT_RULE_COUNT];
+    spw_vspw_fault_rule_t rules[SPW_VSPW_FAULT_RULE_COUNT];
+    uint64_t states[SPW_VSPW_FAULT_RULE_COUNT];
+    uint32_t injected_counts[SPW_VSPW_FAULT_RULE_COUNT];
     uint64_t seed;
 } spw_deterministic_fault_injector_t;
 
-bool spw_fault_rule_valid(const spw_udp_fault_rule_t* rule);
-void spw_fault_injector_init(spw_deterministic_fault_injector_t* injector,
-                             const spw_udp_config_t* config);
+bool spw_fault_rule_valid(const spw_vspw_fault_rule_t* rule);
+void spw_fault_injector_init(
+    spw_deterministic_fault_injector_t* injector,
+    const spw_vspw_fault_rule_t* rules,
+    size_t rule_count,
+    uint64_t seed);
 void spw_fault_injector_reset(spw_deterministic_fault_injector_t* injector);
 spw_fault_decision_t spw_fault_inject_transport(
     spw_deterministic_fault_injector_t* injector,
